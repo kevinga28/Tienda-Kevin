6615322f51a43c5c95b7a37c111c4b1db0833383
kevinga28 <kevinga2806@gmail.com>
2023-05-31 10:21:52 -0600

    Semana 2 Tienda de Kevin Garro Rodriguez

---
+++ src/main/java/TiendaKevin/TiendaKevin/TiendaKevinApplication.java
@@ -0,0 +1,13 @@
+package TiendaKevin.TiendaKevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaKevinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaKevinApplication.class, args);
+	}
+
+}
